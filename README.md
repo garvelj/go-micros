1824fd76e8e771f8b1cc3822b17f296756840bde
borninwinter56 <borninwinter56@gmail.com>
2025-07-03 17:16:13 +0200

    Removed old RabbitMQ data, started the listener from beginning

--- README.md
+++ README.md
@@ -10,3 +10,16 @@ The project can be ran through the make commands
 5. To stop the frontend use make stop
 6. To stop the services use make down
 7. To access the mailhog first start the project than go to localhost:8025
+
+
+If the rabbitMQ isn't able to connect, and it displays the following error message: 
+``` 
+Cookie file /var/lib/rabbitmq/.erlang.cookie must be accessible by owner only
+``` 
+
+Use the following bash command to remove RabbitMQ data from the project:
+```
+rm -rf ./db-data/rabbitmq/
+```
+And then try to build docker compose again, it will most probably solve it.
+
--- listener-service/main.go
+++ listener-service/main.go
@@ -1,5 +1,30 @@
 package main
 
+// Listener service is a service that talks to Rabbit MQ
+// which is a server that manages queues
+
+// What is the actual use case:
+// Someone would want to authenticate,
+// And the broker won't communicate with authentication service directly,
+// Instead, it pushes instructions into RabbitMQ
+// Which is a AMQP - Advanced Messaging Queue Protocol Server
+// RabbitMQ would take that request and add it to the queue
+// And the listener pulls from that queue
+// And and calls the apropriate service based on that.
+
+// So what happens in real life:
+// Broker recieves a request
+// Says "cool, ill just push that to the queue"
+// Then he forgets about it
+// The listener service gets that request from the queue and says
+// "what should i do with this? should i log something, send an email..."
+// In this case it will be the authentication of a user.
+
+// This sounds complicated but actually isn't.
+
+// The driver that is needed for this communication with RabbitMQ
+// github.com/rabbitmq/amqp091-go
+
 import (
 	"fmt"
 	"log"
@@ -11,46 +36,66 @@ import (
 )
 
 func main() {
-	// try to connect to rabbitmq
+	// First we'll try to connect with RabbitMQ
 	rabbitConn, err := connect()
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
+
 	defer rabbitConn.Close()
 
-	log.Println("Connected to RabbitMQ")
-	// start listening
+	// Then start listening for messages
+	// This service won't be fetching the messages
+	// Instead, the queue will push it right to us
+	// We'll listen for certain queues, and once there's an event on those
+	// We get it from the queue
 
-	// create consumer
+	// Create a consumer
 
-	// watch queue and consume events
+	// Watch the queue and consume the events
 }
 
+// So we obviously we have to add the listener service to our docker compose file
+// We have to see an available rabbitmq docker image on docker hub
+// -rc in the tag of the image stands for release candidate, and i'ts usually not safe to use them
+
+// Let's put the connection to the RabbitMQ in it's own function
 func connect() (*amqp.Connection, error) {
-	var counts int64
-	var backoff = 1 * time.Second
-	var connection *amqp.Connection
+	var (
+		counts     int
+		backOff    = 1 * time.Second
+		connection *amqp.Connection
+	)
 
 	for {
+		// Attempt connection
 		c, err := amqp.Dial("amqp://guest:guest@localhost")
 		if err != nil {
-			fmt.Println("Rabbit not yet ready...")
+
+			// if error occured -> print and increase count
+			fmt.Println("RabbitMQ not ready yet...")
+			fmt.Println("Err: ", err)
 			counts++
 		} else {
+
+			// if connection is successful, break from the loop
 			connection = c
 			break
 		}
 
+		// we don't want this to be looping forever
+		// So after a while if it isn't connected
+		// There's clearly a problem
 		if counts > 5 {
 			fmt.Println(err)
 			return nil, err
 		}
 
-		backoff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
-		log.Println("Backing off...")
-		time.Sleep(backoff)
-		continue
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		fmt.Println("Backing off...")
+		time.Sleep(backOff)
 	}
+
 	return connection, nil
 }
